1eb5db8a686800fda8c95f43e32a84fa508a20da
bruno.chabot <bruno.chabot@201214-10.internet.etrs>
2014-04-13 19:04:12 +0200

    mise en place d'une structure de base pour le site Début de la partie de création d'un tournoi

--- BadmintonienCessonaisWeb/src/beans/tournoi/creationTournoiBean.java
+++ BadmintonienCessonaisWeb/src/beans/tournoi/creationTournoiBean.java
@@ -1,9 +1,14 @@
-package beans;
+package beans.tournoi;
 
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 
+import lombok.Data;
+import sessions.tournoi.FacadeTournoi;
+import entities.tournoi.Tournoi;
+
 @ManagedBean
+@Data
 public class creationTournoiBean {
 
 	@EJB
@@ -12,3 +17,4 @@ public class creationTournoiBean {
 	
 	
 }
+
