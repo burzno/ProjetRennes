f27a6b2e1e3b76bfa92bae960197d5a89bc15e30
bruno.chabot <bruno.chabot@201214-10.internet.etrs>
2014-04-17 09:24:21 +0200

    Mise en place du WebService permettant de délivrer les classements FFBA

---
+++ FFBaClassementWS/src/webservice/metier/ClassementFFBA.java
@@ -0,0 +1,26 @@
+package webservice.metier;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
+
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@FieldDefaults(level=AccessLevel.PRIVATE)
+@XmlAccessorType(XmlAccessType.FIELD)
+public class ClassementFFBA {
+
+	
+	@XmlAttribute(name="licence")
+	String licenceFFBA;
+	@XmlElement(name="simple")
+	String matchSimple;
+	@XmlElement(name="double")
+	String matchDouble;
+	@XmlElement(name="doubleMixte")
+	String matchDoubleMixte;
+}
